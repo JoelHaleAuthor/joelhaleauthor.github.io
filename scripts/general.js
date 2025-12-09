21e7d3094495634f2770454168e43c38c5fd1ea1
Joel Hale <tquagmire925@gmail.com>
2025-12-08 16:01:27 -0800

    novel excerpts

--- scripts/general.js
+++ scripts/general.js
@@ -91,22 +91,32 @@ function PopulateSelect(arr, optCtl, optSelect) {
 
 function ExpandImage(img){
     if (img.className == "expandable"){
+        var parentWidth = parseInt(img.parentElement.parentElement.offsetWidth);
         var tp = document.getElementById("page-header").offsetHeight;
         var oCalc = calcImgSize(img, tp);      
         var height = img.style["height"];
         var width = img.style["width"];
+        var left = img.style["left"];
         var maxWid = img.style["max-width"];
         var maxHt = img.style["max-height"];
-        var ht = parseInt(document.body.offsetHeight * .8);
-        var mwd = parseInt(document.body.offsetWidth * .9);
-        var oldStyle = {"height": height, "width": width, "max-width": maxWid, "max-height": maxHt}; 
+        //var ht = parseInt(document.body.offsetHeight * .8);
+        //var mwd = parseInt(document.body.offsetWidth * .9);
+        var oldStyle = {"height": height, "width": width, "max-width": maxWid, "max-height": maxHt, "left": left}; 
+        var newWidth = oCalc["width"];
+        if (newWidth > parentWidth){
+            newWidth = parentWidth - 30;
+        }
+        //var newLeft = (parseInt(oCalc["viewWidth"] - parentWidth) / 2) + img.parentElement.parentElement.offsetLeft - parseInt(newWidth/3);
+        var newLeft = document.body.offsetWidth / 2 - (newWidth / 2);
         img["oldStyle"] = oldStyle;
-        img.style["height"] = oCalc["height"];
-        img.style["width"] = oCalc["width"];
-        img.style["max-width"] = oCalc["width"];
-        img.style["max-height"] = oCalc["height"];
+        img.style["height"] = oCalc["height"] + "px";
+        img.style["width"] = oCalc["width"] + "px";
+        img.style["max-width"] = oCalc["width"] + "px";
+        img.style["max-height"] = oCalc["height"] + "px";
+        img.style["left"]= newLeft + "px";
         img.style["top"] = tp + "px";
         img.className = "expanded";
+        //console.log({ "img.style['width']": img.style["width"], "oCalc": oCalc, newLeft: newLeft });
     }
     else if (img.className == "expanded"){
         var oldStyle = img["oldStyle"];
@@ -114,20 +124,21 @@ function ExpandImage(img){
         img.style["max-height"] = oldStyle["max-height"];
         img.style["width"] = oldStyle["width"];
         img.style["height"] = oldStyle["height"];
+        img.style["left"] = oldStyle["left"];
         img.style["top"] = "";
         img.className = "expandable"; 
     }
 }
 
 function calcImgSize(img, tp){
+    const tagName = img.tagName.toLowerCase();
     const vw = window.innerWidth;
-    const vh = window.innerHeight;
+    const vh = window.innerHeight - 50;
     const availableHeight = vh - tp;
-    const iw = img.naturalWidth;
-    const ih = img.naturalHeight;
+    const iw = (tagName === "img" ? img.naturalWidth : img.offsetWidth - 50);
+    const ih = (tagName === "img" ? img.naturalHeight : img.offsetHeight);
     const scale = Math.min(vw / iw, availableHeight / ih);
-    newWidth = parseInt(iw * scale) + "px";
-    newHeight = parseInt(ih * scale) + "px";
-    return { width: newWidth, height: newHeight  };
+    newWidth = parseInt(iw * scale);
+    newHeight = parseInt(ih * scale);
+    return { width: newWidth, height: newHeight, viewWidth: vw  };
 }
-
